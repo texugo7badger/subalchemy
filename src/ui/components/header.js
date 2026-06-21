a15ff7fa628d4c773079ea1b0ae4cb3dea166bca
texugo7badger <texugo7badger@gmail.com>
2026-06-21 05:05:02 -0300

    fix: correct logo image path to ui/assets folder

--- src/ui/components/header.js
+++ src/ui/components/header.js
@@ -1,6 +1,6 @@
 function header() {
   return `
-<img src="/subalchemy-logo.png" alt="SubAlchemy Logo" class="logo">
+<img src="ui/assets/subalchemy-logo.png" alt="SubAlchemy Logo" class="logo">
 <h1>SubAlchemy</h1>
 <p class="subtitle">Universal SRT Converter for Tizen 9 & Anime</p>
 <p class="description">For the best experience across all your devices, we recommend configuring this addon with the same API keys you use in <a href="https://stremio-addons.net/addons/subsense" target="_blank">SubSense</a>.</p>
